271aad31abf54d9214e79afe2d32df1e2f910bb5
David Paula <davidpaulati@gmail.com>
2021-07-30 21:05:26 -0300

    exemplo do curso de fundamentos da alura

---
+++ console.js
@@ -0,0 +1,10 @@
+//log -> registro
+
+const minhaVar = true;
+
+// console.log(245);
+// console.log("eu sou um texo");
+// console.log(minhaVar);
+
+//tratamento de erro!
+console.error("erro meu");
