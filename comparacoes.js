4ea2a025289402d61c7a03c5eb3d9c2d5d7df919
David Paula <davidpaulati@gmail.com>
2021-07-30 22:08:02 -0300

    exemplo do curso de fundamentos da alura

---
+++ comparacoes.js
@@ -0,0 +1,19 @@
+// == comparação implicita
+
+const numero = 5;
+const texto = "5";
+// console.log(numero == texto);
+// console.log(numero === texto);
+
+//typeof
+console.log(typeof(numero));
+console.log(typeof(texto))
+
+// == compara so o valor 
+// === compara o valor e o tipo de dado
+
+
+//conversão explicita
+
+Number()
+String()
---
+++ template_string.js
@@ -0,0 +1,14 @@
+const nome = "David";
+const idade = 2021 - 1991;
+const cidadeNatal = "Belém";
+
+// const apresentação = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeNatal;
+
+// template string
+const apresentação = `meu nome é ${nome}, miha idade é ${idade} e nasci na cidade de ${cidadeNatal}`
+
+console.log(apresentação);
+
+
+
+
